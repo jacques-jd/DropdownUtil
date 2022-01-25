3bad4053ec06090ad873dd1811ec5e4f2ef5b56d
jacquesjd <jacques.dancause@gmail.com>
2022-01-24 23:26:00 -0400

    Fixed naming
    Bug fixes

--- dropdown.js
+++ dropdown.js
@@ -1,82 +1,101 @@
 //SETTINGS
-// dropdownClass = The class name you will apply to the dropdown objects
-// triggerClass = The class name you will apply to the item to click on, or the "Trigger" item.
-// unopened = On your trigger, you should have an indication whether the dropdown is open or not. This is it when it is closed
-// opened = This is the indication for when the dropdown is open.
-// hoverable = whether or not one can simply hover to trigger the expansion of the dropdown.
+// dropdown.target = The class name you will apply to the dropdown objects
+// dropdown.trigger = The class name you will apply to the item to click on, or the "Trigger" item.
+// dropdown.unopened = On your trigger, you should have an indication whether the dropdown is open or not. This is it when it is closed
+// dropdown.opened = This is the indication for when the dropdown is open.
+// dropdown.hoverable = whether or not one can simply hover to trigger the expansion of the dropdown.
 
 //Note: Each trigger item is associated with the following dropdown item. You need to have the same amount of Triggers and DropDowns.
+let dropdown = {
+    target: 'dropdown',
+    trigger: 'droptrigger',
+    opened: '⮟',
+    unopened: '⮞',
+    hoverable: true,
+};
 
-let dropdownClass = 'drop',
-    triggerClass = 'trig',
-    unopened = '⮞',
-    opened = '⮟',
-    hoverable = true;
 
-
-window.onload = event =>
+window.addEventListener("load", event =>
 {
     let drops = []; //list of all dropdowns with trigger, state, and target
 
-    for (let i = 0; i < document.querySelectorAll(`.${dropdownClass}`).length; i++)
+    for (let i = 0; i < document.querySelectorAll(`.${dropdown.target}`).length; i++)
     {
-        if (!document.querySelectorAll(`.${triggerClass}`)[i]) 
+        if (!document.querySelectorAll(`.${dropdown.trigger}`)[i]) 
         {
-            throw new Error(`Missing trigger for an expandable.`);
+            throw new Error(`Missing trigger for a dropdown.`);
         }
 
-        drops.push({ trigger: document.querySelectorAll(`.${triggerClass}`)[i], target: document.querySelectorAll(`.${dropdownClass}`)[i], open: false });
+        drops.push({ trigger: document.querySelectorAll(`.${dropdown.trigger}`)[i], target: document.querySelectorAll(`.${dropdown.target}`)[i], open: false, hover: false });
     }
 
-    for (let dd of drops)
+    for (let ddp of drops)
     {
         //this line adds the indicators. Modify if you want it in a different format.
-        dd.trigger.innerHTML = `${dd.trigger.innerHTML} ${unopened}`;
-        dd.trigger.style.cursor = "pointer";
+        ddp.trigger.innerHTML = `${ddp.trigger.innerHTML} ${dropdown.unopened}`;
+        ddp.trigger.style.cursor = "pointer";
 
-        //this line hides them and makes them 0 height by default
-        dd.target.style.opacity = "0";
-        dd.target.style.height = "0";
-        dd.target.style.position = "absolute";
-
-        //set backgroundcolor if there is none already set
-        if (getComputedStyle(dd.target).getPropertyValue('background-color') === 'rgba(0, 0, 0, 0)') dd.target.style.backgroundColor = "#FFF";
-
-        //this line adds the transition. comment out if you don't want transition.
-        dd.target.style.transition = "opacity 0.2s, transform 0.2s";
-
-        let open = () =>
+        dropdown.open = dd =>
         {
+            dd.target.style.visibility = "visible";
             dd.target.style.transform = 'scaleY(1)';
-            dd.target.style.opacity = '1';
             dd.target.style.height = 'auto';
 
-            dd.trigger.innerHTML = dd.trigger.innerHTML.replace(unopened, opened);
+            dd.trigger.innerHTML = dd.trigger.innerHTML.replace(dropdown.unopened, dropdown.opened);
 
             dd.open = true;
         }
 
-        let close = () =>
+        dropdown.close = dd =>
         {
             dd.target.style.transform = 'scaleY(0)';
-            dd.target.style.opacity = '0';
-            dd.target.style.height = '0';
 
-            dd.trigger.innerHTML = dd.trigger.innerHTML.replace(opened, unopened);
+            setTimeout(dd=>{
+                if(!dd.hover)
+                {
+                    dd.target.style.height = '0';
+                    dd.target.style.visibility = "hidden";
+                }
+            }, 200, dd);
+            
+            dd.trigger.innerHTML = dd.trigger.innerHTML.replace(dropdown.opened, dropdown.unopened);
 
             dd.open = false;
         }
 
-        let trigger = event =>
+        dropdown.delayedClose = dd =>
         {
-            if (dd.open) close(); else open();
+            setTimeout(() => 
+            {
+                if (!dd.hover) dropdown.close(dd);
+                else dropdown.delayedClose(dd);
+            }, 500);
         }
 
-        if (hoverable) 
+        dropdown.toggle = dd =>
         {
-            dd.trigger.onmouseenter = open;
-            dd.trigger.onmouseleave = close;
+            if (dd.open) dropdown.close(dd); else dropdown.open(dd);
         }
-        else dd.trigger.onclick = trigger;
+
+        //this line hides them and makes them 0 height by default
+        dropdown.close(ddp);
+        ddp.target.style.position = "absolute";
+
+        //prevents it from flashing when loading screen
+        setTimeout(ddp => {
+            ddp.target.style.transition = "transform 0.2s";
+        }, 200, ddp);
+
+        //this line adds the transition. comment out if you don't want transition.
+
+        if (dropdown.hoverable) 
+        {
+            ddp.target.addEventListener("mouseleave", () => { ddp.hover = false; dropdown.delayedClose(ddp); });
+            ddp.trigger.addEventListener("mouseleave", () => { ddp.hover = false; dropdown.delayedClose(ddp); });
+            ddp.target.addEventListener("mouseenter", () => { ddp.hover = true; dropdown.open(ddp); });
+            ddp.trigger.addEventListener("mouseenter", () => { ddp.hover = true; dropdown.open(ddp); });
+        }
+        else ddp.trigger.addEventListener("click", dropdown.toggle);
+        
     }
-}
+});
